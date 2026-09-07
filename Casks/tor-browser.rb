cdd2becde4ca3b90fd06e88c7b1bc89b64f0c2ef
egigoka <egigoka@gmail.com>
2026-09-07 13:59:52 +0500

    Remove deprecated verified parameter from url stanza

--- Casks/tor-browser.rb
+++ Casks/tor-browser.rb
@@ -2,8 +2,7 @@ cask "tor-browser" do
   version "15.0.21"
   sha256 "2adc3e7392c371475d4e857c8c1a7411d477eeab459d886195afea98c3a4572d"
 
-  url "https://github.com/egigoka/homebrew-tor-browser-mirror/releases/download/v#{version}/tor-browser-macos-#{version}.dmg",
-      verified: "github.com/egigoka/homebrew-tor-browser-mirror/"
+  url "https://github.com/egigoka/homebrew-tor-browser-mirror/releases/download/v#{version}/tor-browser-macos-#{version}.dmg"
   name "Tor Browser"
   desc "Anonymous web browser based on Firefox (mirrored from torproject.org)"
   homepage "https://www.torproject.org/"
